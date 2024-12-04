602b0fe75175e4041614372167d73d7a7a53a48e
Stive627 <fossistive627@gmail.com>
2024-12-05 01:40:24 +0530

    third commit

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const authRoutes = require('./authRoutes')
+// The url to reach the google developper : https://console.developers.google.com/
 const app = express()
 app.use('/', authRoutes)
 app.get('/', authRoutes.get(''), (req, res)=>{
